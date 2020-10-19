b026d08e68d04d974f647372fe4c539e35889bf3
Margarida Martelo <margmartelo@gmail.com>
2020-10-19 17:54:09 +0100

    Second test: testing for number 5 as input and "buzz" as output.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,12 @@
 require 'fizzbuzz'
 
+# first rspec test: returns fizz when is used as an argument
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
   end
 end
