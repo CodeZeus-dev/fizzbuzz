3b95668449db074c6195cd4c8bc95bd38b25edcb
CodeZeus-dev <constantine.akritides@gmail.com>
2020-10-20 18:00:14 +0100

    Modified the fizzbuzz Main object method so that it contains a commented alternative one-line version

--- lib/fizzbuzz_own.rb
+++ lib/fizzbuzz_own.rb
@@ -1,5 +1,4 @@
 def fizzbuzz_own(number)
-  # return number % 3 == 0 && number % 5 == 0 ? 'fizzbuzz' : number % 3 == 0 ? 'fizz' : number % 5 == 0 ? 'buzz' : number
   if number % 3 == 0 && number % 5 == 0
     'fizzbuzz'
   elsif number % 3 == 0
@@ -9,4 +8,6 @@ def fizzbuzz_own(number)
   else
     number
   end
+  # Alternative one-line solution:
+  # return number % 3 == 0 && number % 5 == 0 ? 'fizzbuzz' : number % 3 == 0 ? 'fizz' : number % 5 == 0 ? 'buzz' : number
 end
